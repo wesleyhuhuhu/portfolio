9c62b900335b03b412c0519e0d5d9cf4050dc9f5
wesleyhuhuhu <31289917+wesleyhuhuhu@users.noreply.github.com>
2023-07-04 21:00:44 -0700

    fixed some blur bugs

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const navigation = document.querySelector('.navigation');
+const blurEl = document.querySelector('#blur');
+const header = document.querySelector('header');
+const nameBackdrop = document.querySelector('#sticky');
+const empty = document.querySelector('#empty');
+let nameBackdropstyle = nameBackdrop.currentStyle || window.getComputedStyle(nameBackdrop);
+
 
 const observer = new IntersectionObserver((entries) => {
+    const scrollTop = document.body.scrollTop;
     entries.forEach((entry) => {
         
         if (entry.isIntersecting) {
@@ -11,14 +19,74 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-const navigation = document.querySelector('.navigation');
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
 
 addEventListener('scroll', e => {
     const scrollTop = document.body.scrollTop;
-    console.log(scrollTop);
+    console.log('scroll: ' + scrollTop);
     const translate = scrollTop * 3;
+    const blur = scrollTop / 300;
     navigation.style.transform = `translateX(-${translate}px)`;
     console.log(navigation.style.transform);
+    console.log('width: ' + window.innerWidth);
+    if (scrollTop > window.innerWidth/2) {
+        header.style.opacity = 0;
+    }
+    else {
+        header.style.opacity = 1;
+    }
+})
+
+addEventListener('scroll', e => {
+    const scrollTop = document.body.scrollTop;
+    var toTop = nameBackdrop.offsetTop;
+    console.log('to: ' + toTop);
+
+    console.log(scrollTop);
+    console.log(window.innerHeight);
+    if (scrollTop > (window.innerHeight)/3) {
+        blurEl.style.backdropFilter = 'blur(12px)';
+        
+        console.log('now');
+    }
+    else {
+        blurEl.style.backdropFilter = '';
+    }
+
+    if (scrollTop >= window.innerHeight) {
+        nameBackdrop.style.marginLeft = '0';
+        nameBackdrop.style.marginRight = '0';
+    }
+    else if (scrollTop >= toTop) {
+        nameBackdrop.style.marginLeft = '20vw';
+        nameBackdrop.style.marginRight = '20vw';
+    }
+    else {
+        nameBackdrop.style.marginLeft = '32vw';
+        nameBackdrop.style.marginRight = '32vw';
+    }
+
+    // blurMarginLeft = parseInt(nameBackdropstyle.marginLeft);
+    // blurMarginRight = parseInt(nameBackdropstyle.marginRight);
+    // console.log('marginleft: ' + blurMarginLeft);
+    // console.log('marginright: ' + blurMarginRight);
+    // blurMarginLeft++;
+    // blurMarginRight++;
+    // blurMarginLeft = blurMarginLeft + 'px';
+    // blurMarginRight = blurMarginRight + 'px';
+    // console.log('marginleft: ' + blurMarginLeft);
+    // console.log('marginright: ' + blurMarginRight);
+    // nameBackdrop.marginLeft = blurMarginLeft;
+    // nameBackdrop.blurMarginRight = blurMarginRight;
 })
 
 const hiddenElements = document.querySelectorAll('.hidden');
